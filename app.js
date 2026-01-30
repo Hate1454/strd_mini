cb018557bc1a34d5bc7eb7596af2aa820f8d3204
Hate1454 <makson385@gmail.com>
2026-01-30 12:46:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const API_URL = 'https://sigma.strd.ru/pcgi/api/product4.pl';
 
 fetch(API_URL)
-  .then(r => r.json())
+  .then(r => {
+    console.log('HTTP status:', r.status);
+    return r.json();
+  })
   .then(data => {
-    if (!data.success) return;
+    console.log('API data:', data);
+
+    if (!data.success) {
+      console.error('API success=false');
+      return;
+    }
 
     const root = document.getElementById('products');
+
+    if (!data.items.length) {
+      root.innerHTML = '<p>Товары не найдены</p>';
+      return;
+    }
 
     data.items.forEach(p => {
       const div = document.createElement('div');
@@ -19,4 +32,7 @@ fetch(API_URL)
 
       root.appendChild(div);
     });
+  })
+  .catch(err => {
+    console.error('FETCH ERROR:', err);
   });
